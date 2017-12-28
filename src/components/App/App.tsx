8d1f853ee22164919c20af61ae2afd072b2931bd
zhaopeng <719197352@qq.com>
2017-12-28 10:08:46 +0800

    new branch

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Button } from 'antd'
+import './App.css'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <Button type="primary">Button</Button>
+      </div>
+    )
+  }
+}
+
+export default App
